a32b0602e10503553f869832ff9e824a2a00298d
george.barbu <barbu.george89@gmail.com>
2025-03-20 10:57:01 +0100

    fix responsive header

--- components/shared/AppHeader.jsx
+++ components/shared/AppHeader.jsx
@@ -31,12 +31,12 @@ function AppHeader() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       id="nav"
-      className="sm:container sm:mx-auto"
+      className="lg:container lg:mx-auto"
     >
       {/* Header */}
-      <div className="z-10 max-w-screen-lg xl:max-w-screen-xl block sm:flex sm:justify-between sm:items-center py-6">
+      <div className="z-10 max-w-screen-lg xl:max-w-screen-xl block lg:flex lg:justify-between lg:items-center py-6">
         {/* Header menu links and small screen hamburger menu */}
-        <div className="flex justify-between items-center px-4 sm:px-0">
+        <div className="flex justify-between items-center px-4 lg:px-0">
           <div>
             <Link href="/">
               {mounted && (
@@ -44,14 +44,14 @@ function AppHeader() {
                   src={logoSrc}
                   className="cursor-pointer"
                   alt={activeTheme === "dark" ? "Dark Logo" : "Light Logo"}
-                  width={225}
+                  width={210}
                   height={120}
                 />
               )}
             </Link>
           </div>
 
-          <div className="flex flex-end items-center justify-center  sm:hidden ">
+          <div className="flex flex-end items-center justify-center lg:hidden ">
             {/* Small screen hamburger menu */}
             <button
               onClick={toggleMenu}
@@ -92,31 +92,31 @@ function AppHeader() {
         <div
           className={
             showMenu
-              ? "block m-0 sm:ml-4 sm:mt-3 md:flex px-5 py-3 sm:p-0 justify-between items-center shadow-lg sm:shadow-none"
+              ? "block m-0 lg:ml-4 lg:mt-3 lg:flex px-5 py-3 lg:p-0 justify-between items-center shadow-lg lg:shadow-none"
               : "hidden"
           }
         >
-          <div className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2">
+          <div className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  lg:mx-4 mb-2 lg:py-2">
             <Link href="/projects" aria-label="Projects">
               Projects
             </Link>
           </div>
-          <div className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark">
+          <div className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  lg:mx-4 mb-2 lg:py-2 border-t-2 pt-3 lg:pt-2 lg:border-t-0 border-primary-light dark:border-secondary-dark">
             <Link href="/about" aria-label="About Me">
               About Me
             </Link>
           </div>
-          <div className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark">
+          <div className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  lg:mx-4 mb-2 lg:py-2 border-t-2 pt-3 lg:pt-2 lg:border-t-0 border-primary-light dark:border-secondary-dark">
             <Link href="/contact" aria-label="Contact">
               Contact
             </Link>
           </div>
-          <div className="border-t-2 pt-3 sm:pt-0 sm:border-t-0 border-primary-light dark:border-secondary-dark">
+          <div className="border-t-2 pt-3 lg:pt-0 lg:border-t-0 border-primary-light dark:border-secondary-dark">
             <button
               onClick={() =>
                 window.open(process.env.NEXT_PUBLIC_CV_SITE_URL, "_blank")
               }
-              className="font-general-medium sm:hidden block text-left text-md bg-blue-500 hover:bg-blue-600 text-white shadow-sm rounded-sm px-4 py-2 mt-2 duration-300 w-32"
+              className="font-general-medium lg:hidden block text-left text-md bg-blue-500 hover:bg-blue-600 text-white shadow-sm rounded-sm px-4 py-2 mt-2 duration-300 w-32"
               aria-label="My CV Button"
             >
               My Resume
@@ -125,22 +125,22 @@ function AppHeader() {
         </div>
 
         {/* Header links large screen */}
-        <div className="font-general-medium hidden m-0 sm:ml-4 mt-5 sm:mt-3 sm:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none">
+        <div className="font-general-medium hidden m-0 lg:ml-4 mt-5 lg:mt-3 lg:flex p-5 lg:p-0 justify-center items-center shadow-lg lg:shadow-none">
           <div
-            className="block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
+            className="block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  lg:mx-4 mb-2 lg:py-2"
             aria-label="Projects"
           >
             <Link href="/projects">Projects</Link>
           </div>
           <div
-            className="block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
+            className="block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  lg:mx-4 mb-2 lg:py-2"
             aria-label="About Me"
           >
             <Link href="/about">About Me</Link>
           </div>
 
           <div
-            className="block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
+            className="block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  lg:mx-4 mb-2 lg:py-2"
             aria-label="Contact"
           >
             <Link href="/contact">Contact</Link>
@@ -148,8 +148,8 @@ function AppHeader() {
         </div>
 
         {/* Header right section buttons */}
-        <div className="hidden sm:flex justify-between items-center flex-col md:flex-row">
-          <div className="hidden md:flex">
+        <div className="hidden lg:flex justify-between items-center flex-col lg:flex-row">
+          <div className="hidden lg:flex">
             <button
               onClick={() =>
                 window.open(process.env.NEXT_PUBLIC_CV_SITE_URL, "_blank")
